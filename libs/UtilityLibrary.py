34b04360fa8a19eb802066ef690de560d8e6a31c
Chenyang Li <cli049@uottawa.ca>
2025-02-02 18:50:28 -0500

    Added UtilityLibrary.py

---
+++ libs/UtilityLibrary.py
@@ -0,0 +1,15 @@
+import os
+
+class UtilityLibrary:
+    def concatenate_strings(self, *args):
+        """Concatenates multiple strings into one."""
+        return ' '.join(args)
+
+    def split_string(self, string, delimiter=' '):
+        """Splits a string by the given delimiter."""
+        return string.split(delimiter)
+
+    def get_file_extension(self, filename):
+        """Returns the file extension of the given filename."""
+        return os.path.splitext(filename)[1]
+
